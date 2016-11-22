b0b6bb6fd0124191b80a437891a89256ad15bd88
Michael Gutteridge <michael.gutteridge@gmail.com>
2016-11-22 15:12:02 -0800

    dont need wrapper script

--- ipypar/do_sim.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ipython3
-
-import numpy as np
-
-def do_sim():
-    a = np.random.rand(10,1)
-    b = np.random.rand(100,1)
-    c = (a[1]+b[1]) * 1000
-    return { 'a': a, 'b': b, 'c':c }
